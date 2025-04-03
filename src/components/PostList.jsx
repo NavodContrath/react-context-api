ce45f6f445e0a655874c7a71d0f11f117b235860
NavodContrath <navod.contrath@gmail.com>
2025-04-03 16:50:36 +0200

    implement PostList component to display posts using PostContext

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,7 +1,24 @@
+import { useContext } from "react"
+import PostContext from "../contexts/PostContext"
+import PostCard from "./PostCard"
 export default function PostList() {
+    const { posts } = useContext(PostContext)
+    console.log(posts)
+    if (!posts || posts.length === 0) {
+        return <h3>Loading posts...</h3>;
+    }
     return (
         <>
             <div className="row">
+                {
+                    posts.map(post => {
+                        return (
+
+                            <PostCard key={post.id} post={post} />
+
+                        )
+                    })
+                }
 
             </div>
         </>
