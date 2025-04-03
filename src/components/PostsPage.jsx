9c7616a6933ed761eeb07e53d3c20490d7070c77
NavodContrath <navod.contrath@gmail.com>
2025-04-03 16:10:49 +0200

    add PostsPage component with basic structure

---
+++ src/components/PostsPage.jsx
@@ -0,0 +1,12 @@
+export default function PostPage() {
+    return (
+        <>
+            <div className="container">
+                <div>
+                    <h1>Post List</h1>
+                </div>
+            </div>
+
+        </>
+    )
+}
