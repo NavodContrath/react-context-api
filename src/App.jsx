75d0c1f99ed4249524d771295552923f082f4b76
NavodContrath <navod.contrath@gmail.com>
2025-04-03 16:08:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <nav className="navbar navbar-expand-sm navbar-light bg-secondary">
+          <div className="container">
+            <a className="navbar-brand" href="#">Context</a>
+            <div className="collapse navbar-collapse" id="collapsibleNavId">
+              <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#" aria-current="page">Home </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+              <form className="d-flex my-2 my-lg-0">
+                <input className="form-control me-sm-2" type="text" placeholder="Search" />
+                <button className="btn btn-info my-2 my-sm-0" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <nav className="navbar fixed-bottom navbar-expand-sm  bg-secondary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Context.inc°</a>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </footer>
+
+    </>
+  )
+}
+
+export default App
