8bfe7597087fe0600ebb5b69d029a6bcbcb60f12
NavodContrath <navod.contrath@gmail.com>
2025-04-03 16:13:30 +0200

    add PostList component to PostsPage for displaying posts

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,9 +1,11 @@
+import PostList from "./PostList"
 export default function PostPage() {
     return (
         <>
             <div className="container">
                 <div>
                     <h1>Post List</h1>
+                    <PostList />
                 </div>
             </div>
 
